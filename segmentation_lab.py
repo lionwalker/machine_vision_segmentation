63cf4dda7ec215db6742df659fef364295df590b
Lahiru Madhusanka <hhlahirumadusanka@gmail.com>
2026-02-04 12:04:20 +0200

    Add robodk image histogram and clean code

--- segmentation_lab.py
+++ segmentation_lab.py
@@ -7,9 +7,6 @@ INPUT_FILE = "images/robodk_imgN.png"
 OUTPUT_FILE = "images/robodk_imgN_annotated.png"
 
 img = cv2.imread(INPUT_FILE, cv2.IMREAD_GRAYSCALE)
-
-# hist_cv = cv2.calcHist([img], [0], None, [256], [0, 256])
-
 _, th_binary = cv2.threshold(img, 250, 255, cv2.THRESH_BINARY_INV)
 
 num_labels, labeled_img, stats, centroids = cv2.connectedComponentsWithStats(
@@ -46,51 +43,3 @@ for (p1x, p1y, size_x, size_y, _) in stats[1:]:
 
 cv2.imwrite(OUTPUT_FILE, img)
 print(f"Successfully saved annotated image to {OUTPUT_FILE}")
-
-'''
-__, th_otsu = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-th_mean = cv2.adaptiveThreshold(
-    img, 255,
-    cv2.ADAPTIVE_THRESH_MEAN_C,
-    cv2.THRESH_BINARY,
-    blockSize=5,
-    C=2)
-th_gauss = cv2.adaptiveThreshold(
-    img, 255,
-    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-    cv2.THRESH_BINARY,
-    blockSize=5,
-    C=2)
-
-plt.figure(figsize=(12, 6))
-
-plt.subplot(3, 2, 1)
-plt.imshow(img, cmap='gray', vmin=0, vmax=255)
-plt.title('Image')
-
-plt.subplot(3, 2, 2)
-plt.vlines(range(256), 0, hist_cv)
-plt.title('Histogram')
-plt.xlabel('Intensity')
-plt.ylabel('Frequency')
-plt.xlim([0, 256])
-
-plt.subplot(3, 2, 3)
-plt.imshow(th_binary, cmap='gray', vmin=0, vmax=255)
-plt.title('Binary Thresholding')
-
-plt.subplot(3, 2, 4)
-plt.imshow(th_otsu, cmap='gray', vmin=0, vmax=255)
-plt.title('Otsu Thresholding')
-
-plt.subplot(3, 2, 5)
-plt.imshow(th_mean, cmap='gray', vmin=0, vmax=255)
-plt.title('Adaptive Thresholding (Mean)')
-
-plt.subplot(3, 2, 6)
-plt.imshow(th_gauss, cmap='gray', vmin=0, vmax=255)
-plt.title('Adaptive Thresholding (Gauss)')
-
-plt.tight_layout()
-plt.show()
-'''
